b3e59cd477f423ff3f7ad01a367323c439810155
edustream <edustream1@gmail.com>
2024-04-24 15:18:51 +0200

    php
    modif php

--- src/index.php
+++ src/index.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="styles.css">
-  <title>Vanilla JavaScript App</title>
-</head>
 
 <?php include 'header.php'; ?>
 <br>
